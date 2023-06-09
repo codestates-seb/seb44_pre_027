c28cb908388d20e025ab2e9d58640e962a4b62cd
xion <calf-love@naver.com>
2023-06-09 18:12:12 +0900

    [FIX]change import name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react';
-import './App.css';
+interface AppProps {}
 
-function App() {
+const App = ({}: AppProps) => {
   return (
     <div className="App">
       <div className=" flex items-center justify-center bg-red-300">it goes Work!!</div>
     </div>
   );
-}
+};
 
 export default App;
