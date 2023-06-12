bd9d7b36305d4cd295eb7c67a9a6f905e842e93b
minseong3 <alstjd1021@naver.com>
2023-06-12 17:50:57 +0900

    BE 툴설정

---
+++ src/test/java/com/stackoverflow/stackoverflowclone/StackoverflowCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackoverflow.stackoverflowclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
