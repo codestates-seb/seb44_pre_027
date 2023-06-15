2c45f63041ac594194ed9d8afe4ccfafc07c1f1b
[haru lee] <hitenshi1997@gmail.com>
2023-06-15 11:46:40 +0900

    feat: added answer entity and comment entity

---
+++ server/src/main/java/com/stackoverflow/stackoverflowclone/comment/entity/Comment.java
@@ -0,0 +1,26 @@
+package com.stackoverflow.stackoverflowclone.comment.entity;
+
+import com.stackoverflow.stackoverflowclone.audit.Auditable;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+public class Comment extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(length = 5000, nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+}
