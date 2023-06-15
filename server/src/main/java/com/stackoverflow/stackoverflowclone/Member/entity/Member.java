d96bb407f0e3b73054b04ff1b9c4ced1a02a4552
minseong3 <alstjd1021@naver.com>
2023-06-15 11:45:54 +0900

    Member entity 구현

--- server/src/main/java/com/stackoverflow/stackoverflowclone/Member/entity/Member.java
+++ server/src/main/java/com/stackoverflow/stackoverflowclone/Member/entity/Member.java
@@ -1,2 +1,35 @@
-package com.stackoverflow.stackoverflowclone.Member.entity;public class Member {
+package com.stackoverflow.stackoverflowclone.Member.entity;
+
+import com.stackoverflow.stackoverflowclone.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+
+    private String location;
+
+    private String bio_title;
+
+    private String bio_content;
+
 }
