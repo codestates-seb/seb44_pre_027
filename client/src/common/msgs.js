2c3db468a5666120085f6cd27864df99f17ed8cc
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-06-15 20:05:52 +0900

    data/RSBar에 사용되는 문구 data 생성

---
+++ client/src/common/msgs.js
@@ -0,0 +1,77 @@
+export const RSideBarWidgetData = {
+    Basic:[
+        {
+            id: 1,
+            title: 'The Overflow Blog',
+            content: [
+                "2023 Developer Survey results are in: the latest trends in technology and work from the Stack Overflow community",
+                "Hype or not? Al's benefits for developers explored in the 2023 Developer Survey"
+            ]
+        },
+        {
+            id: 2,
+            title: 'Featured on Meta',
+            content: [
+                'Statement from SO: June 5, 2023 Moderator Action',
+                'Planned maintenance scheduled for Thursday, June, 15, 2023 at 21:00 UTC',
+                'Does the policy change for AI-generated content affect users who (want to) flag such content?',
+                'Temporary policy: ChatGPT is banned'
+            ]
+        },
+        {
+            id: 3,
+            title: 'Hot Meta Posts',
+            content: [
+                'Moderation Strike update: Data dumps, choosing representatives, GPT data, and the policy change for AI-generated content affect users',
+                'What is the impact of the moderation strike?'
+            ]
+        }
+    ],
+    Light: [
+        {
+            id: 1,
+            title: 'Custom Filters',
+        },
+        {
+            id: 2,
+            title: 'Watched Tags',
+            content: 'Watch tags to curate your list of questions.'
+        },
+        {
+            id: 3,
+            title: 'Ignored Tags',
+        },
+        {
+            id: 4,
+            title: 'Collectives',
+            content: [
+                {
+                    id: 1,
+                    title: 'AWS',
+                    content: "Amazone Web Services (AWS) is the world's most comprehensive and broadly adopted blablabla"
+                },
+                {
+                    id: 2,
+                    title: 'Microsoft Azure',
+                    content: 'On-premises, hybrid, multicloud, or at the edge―build on your terms with best-in-class blablabla'
+                },
+                {
+                    id: 3,
+                    title: 'CI/CD',
+                    content: 'A collective where developers focused on continuous integration, delivery, and blablabla'
+                }
+            ]
+        }
+    ]
+}
+
+export const HotNetworkQuestions = [
+    'How can I land without any propulsion? Was the Microsoft simulator right?',
+    'Special parliament that controls and oversees the Justice and Law enforcement systems',
+    "Is it ok that an oven circuit's wire is much larger than the oven's wire?",
+    'Why do sinusoids have DFT magnitudes of N / 2 while we typically normalize by N?',
+    "Should I ever be influenced by an argument that I can't understand?",
+    'Omitted from flight manifest',
+    '17 going on 30 movie with an "aging machine"?',
+    'How to turn a 3D polytope into a mesh?'
+]
