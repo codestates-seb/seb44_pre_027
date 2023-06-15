dc5a5dc25708b560c09ff79f30c572fc208d70f1
[haru lee] <hitenshi1997@gmail.com>
2023-06-15 20:37:15 +0900

    feature : 더미데이터 스크립트 추가

--- server/src/main/java/com/stackoverflow/stackoverflowclone/answer/mapper/AnswerMapper.java
+++ server/src/main/java/com/stackoverflow/stackoverflowclone/answer/mapper/AnswerMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AnswerMapper {
     Answer answerPostToAnswer(AnswerDto.Post requestBody);
     Answer answerPatchToAnswer(AnswerDto.Patch requestBody);
