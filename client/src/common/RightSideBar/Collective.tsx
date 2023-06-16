9fcf6cbc7334d5788213fa4e4c6b40b86edb0233
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-06-16 13:55:25 +0900

    Feat/Collective 컴포넌트 완성(이미지 제외)

--- client/src/common/RightSideBar/Collective.tsx
+++ client/src/common/RightSideBar/Collective.tsx
@@ -8,32 +8,79 @@ interface CollectiveProps{
 }
 
 const Collective = ({item}:CollectiveProps) => {
+    const Text = css`
+        font-size: 13px;
+        color: #3B4045;
+    `;
+
     const CollectiveContainer = styled.div`
         height: 100%;
         padding: 16px 15px;
     `;
 
     const ProfileContainer = styled.div`
-        display: flex;
+        display: grid;
+        grid-template-columns: 1fr 3fr 1fr;
         gap: 12px;
+        margin-bottom: 12px;
     `
 
     const Intro = styled.span`
-        height: 33.98px;
+        height: 40px;
+        ${Text};
 
         display: inline-block;
         overflow: hidden;
         text-overflow: ellipsis;
         word-wrap: break-word;
         /* white-space: norwrap; */
-        font-size: 13px;
-        color: #3B4045;
+    `;
+
+    const Image = styled.img`
+        width: 100%;
+        background-color: red;
+    `
+
+    const TitleContainer = styled.div`
+        height: 31.99px;
+        display : flex;
+        flex-direction: column;
+    ` ;
+
+    const Title = styled.div`
+        color: #6A737C;
+        font-size: 15px;
+        cursor: pointer;
+    `;
+
+    const Caption = styled.div`
+        ${Text};
+    `;
+
+    const Button = styled.button`
+        width: fit-content;
+        padding: 9.6px;
+
+        font-size: 12px;
+        color: #0074cc;
+        background-color: white;
+        border: 1px solid #379fef;
+        border-radius: 3px;
+        &:hover {
+            background-color: #f0f8ff;
+            color: #0063bf;
+        }
     `;
 
     return(
         <CollectiveContainer>
             <ProfileContainer>
-                <img style={{width: '31.99px', height: '30px', backgroundColor:'red'}}></img>
+                <Image/>
+                <TitleContainer>
+                    <Title>{item.title}</Title>
+                    <Caption>{Math.floor(Math.random()*13)}k Members</Caption>
+                </TitleContainer>
+                <Button>Join</Button>
             </ProfileContainer>
             <Intro>{item.content}</Intro>
         </CollectiveContainer>
