961acb79a53971b1c831723945f2d4b0cf202d88
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-06-16 13:56:01 +0900

    Feat/Ad 불러오기 완성

--- client/src/common/RightSideBar/RightSideBar.tsx
+++ client/src/common/RightSideBar/RightSideBar.tsx
@@ -5,6 +5,8 @@ import Widget from './Widget';
 import BasicWidgetItem from './BasicWidgetItem';
 import LightWidgetItem from './LightWidgetItem';
 
+import SideBarAd from './SideBarAd.js';
+
 export interface BasicWidgetItem {
     id:number;
     url:string;
@@ -62,8 +64,9 @@ const RightSideBar = ({size}: RightSideBarProps):JSX.Element => {
                     </Widget>
                 )
             })
-
             }
+
+            <SideBarAd url={'https://pagead2.googlesyndication.com/pcs/click?xai=AKAOjstUZlwdAWpcteFu8YWzaSQdJYhCCH4syNQsrkmVMyzF6-VHJ9ryr6cj8aBfVP99ThJjs931yVhRqmSqysSDOFnTWAZnfDfaXxsGSn50YDTSzB5fB9nzlGPa_07DLx--QDhFGECvne8BzWQ9Tc9HdDB7hOrfrghLEZ8Gpuw6BOzkr06-GA42zShPhl9RahQmrhljwPAId0yIaBbIhyFWPtJ60SZAKnNsEKm5YKSpJEVNYqpbqlal-vWTOnRy04_HrDunKWoQVOB2K1NutoMvw382lZFRMY9yAkuhhszD9ql2Kubb-uGUXLFOSKPeVARn-a_VkFfSFOerVHRM9E7Uelu_NB4T_MOyHDs&sig=Cg0ArKJSzGmw6ILkTK87&fbs_aeid=[gw_fbsaeid]&adurl=https://survey.stackoverflow.co/2023/%3Futm_source%3Dhouse-ads%26utm_medium%3Ddisplay%26utm_campaign%3Ddev-survey-results-2023%26utm_content%3Dsurvey-results&nm=30&nx=150&ny=-70&mb=2'} src={'https://tpc.googlesyndication.com/simgad/2894417314404102158'}/>
         </RightSideBarContainer>
     )
 }
