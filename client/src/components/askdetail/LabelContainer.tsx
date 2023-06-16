d545e3f5dd1fa8be52c645229469e9a401c5f943
xion <calf-love@naver.com>
2023-06-16 10:40:03 +0900

    [CREATE]QuestionScoreComponent and dummydata

--- client/src/components/askdetail/LabelContainer.tsx
+++ client/src/components/askdetail/LabelContainer.tsx
@@ -1,13 +1,13 @@
 import PencilIcon from '@/assets/icons/PencilIcon';
 import SmallPencilIcon from '@/assets/icons/SmallPencilIcon';
 import LabelItem, { LabelItemArray } from './LabelItem';
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
-interface LabelContainerProps {}
+interface LabelContainerProps extends HTMLAttributes<HTMLElement> {}
 
-const LabelContainer = ({}: LabelContainerProps) => {
+const LabelContainer = ({ ...attributes }: LabelContainerProps) => {
   return (
-    <section>
+    <section {...attributes}>
       {LabelItemArray.map((list, idx) => (
         <LabelItem
           key={`LabelItem${idx}`}
--- client/src/pages/question/[id]/QuestionDetailPage.tsx
+++ client/src/pages/question/[id]/QuestionDetailPage.tsx
@@ -2,6 +2,10 @@ import CommentContainer from '@/components/askdetail/CommentContainer';
 import DetailTitle from '@/components/askdetail/DetailTitle';
 import LabelContainer from '@/components/askdetail/LabelContainer';
 import QuestionAnswerComponent from '@/components/askdetail/QuestionAnswerComponent';
+import QuestionScoreConatiner, {
+  questionItemArray,
+  relatedItemArray,
+} from '@/components/askdetail/QuestionScoreConatiner';
 import VoteContainer from '@/components/askdetail/VoteContainer';
 import React from 'react';
 
@@ -12,9 +16,13 @@ const QuestionDetailPage = ({}: QuestionDetailPageProps) => {
     <main>
       <DetailTitle />
 
-      <div className=" flex">
+      <div className="flex">
         <QuestionAnswerComponent />
-        <LabelContainer />
+        <div className=" mt-4 flex flex-col">
+          <LabelContainer className=" mb-4" />
+          <QuestionScoreConatiner title="Linked" itemArray={questionItemArray} />
+          <QuestionScoreConatiner title="Related" itemArray={relatedItemArray} />
+        </div>
       </div>
     </main>
   );
