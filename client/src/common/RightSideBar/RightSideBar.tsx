248a0669daa7aa6baf7ee43d7458c17e228140bf
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-06-16 14:22:17 +0900

    Feat/Hot Network Question 완료

--- client/src/common/RightSideBar/RightSideBar.tsx
+++ client/src/common/RightSideBar/RightSideBar.tsx
@@ -1,5 +1,5 @@
 import { css, styled } from 'styled-components';
-import { RSideBarWidgetData } from '../msgs.js';
+import { RSideBarWidgetData, HotNetworkQuestions } from '../msgs.js';
 
 import Widget from './Widget';
 import BasicWidgetItem from './BasicWidgetItem';
@@ -43,6 +43,45 @@ const RightSideBar = ({size}: RightSideBarProps):JSX.Element => {
         width: ${size}px;
     `
 
+    const Title = styled.h4`
+        margin: 0 0 19px;
+
+        font-size: 19px;
+        color: #3b4045;
+    `
+
+    const NetworkQuestionsContainer = styled.div`
+        display: flex;
+        margin: 0 0 10px;
+    `;
+
+    const Favicon = styled.img`
+        width: 16px;
+        height: 16px;
+
+        margin: 2px 6px 0 0;
+        border: solid 2px red;
+    `
+
+    const HyperLink = styled.a`
+        width: 100%;
+        height: fit-content;
+
+        display: inline-block;
+        cursor: pointer;
+
+        font-size: 12px;
+        color: #0074CC;
+
+        overflow: hidden;
+        text-overflow: ellipsis;
+        word-wrap: break-word;
+
+        &:hover{
+            color: #0a95ff;
+        }
+    `
+
     return(
         <RightSideBarContainer size={size}>
             {RSideBarWidgetData.Basic.map((data:BasicWidgetData):JSX.Element => {
@@ -67,6 +106,16 @@ const RightSideBar = ({size}: RightSideBarProps):JSX.Element => {
             }
 
             <SideBarAd url={'https://pagead2.googlesyndication.com/pcs/click?xai=AKAOjstUZlwdAWpcteFu8YWzaSQdJYhCCH4syNQsrkmVMyzF6-VHJ9ryr6cj8aBfVP99ThJjs931yVhRqmSqysSDOFnTWAZnfDfaXxsGSn50YDTSzB5fB9nzlGPa_07DLx--QDhFGECvne8BzWQ9Tc9HdDB7hOrfrghLEZ8Gpuw6BOzkr06-GA42zShPhl9RahQmrhljwPAId0yIaBbIhyFWPtJ60SZAKnNsEKm5YKSpJEVNYqpbqlal-vWTOnRy04_HrDunKWoQVOB2K1NutoMvw382lZFRMY9yAkuhhszD9ql2Kubb-uGUXLFOSKPeVARn-a_VkFfSFOerVHRM9E7Uelu_NB4T_MOyHDs&sig=Cg0ArKJSzGmw6ILkTK87&fbs_aeid=[gw_fbsaeid]&adurl=https://survey.stackoverflow.co/2023/%3Futm_source%3Dhouse-ads%26utm_medium%3Ddisplay%26utm_campaign%3Ddev-survey-results-2023%26utm_content%3Dsurvey-results&nm=30&nx=150&ny=-70&mb=2'} src={'https://tpc.googlesyndication.com/simgad/2894417314404102158'}/>
+
+
+            <Title>Hot Network Questions</Title>
+            {HotNetworkQuestions.map((item:string)=>{
+                return (<NetworkQuestionsContainer>
+                    <Favicon/>
+                    <HyperLink>{item}</HyperLink>
+                    </NetworkQuestionsContainer>)
+            })}
+
         </RightSideBarContainer>
     )
 }
