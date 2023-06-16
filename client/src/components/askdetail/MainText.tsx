ff73f88eabc1cdf65a226593cbe1bcfca6acb915
xion <calf-love@naver.com>
2023-06-16 11:53:44 +0900

    [CREATE]Edit and Delete button

--- client/src/components/askdetail/MainText.tsx
+++ client/src/components/askdetail/MainText.tsx
@@ -4,11 +4,19 @@ interface MainTextProps {}
 
 const MainText = ({}: MainTextProps) => {
   return (
-    <div className="">
-      I'm having the issue that's faced in this question: Invalid options object. Dev Server has
-      been initialized using an options object that does not match the API schema One of the
-      solutions is to downgrade react-scripts to 4.0.3 and I'm not sure how to do that. Could
-      someone help me?
+    <div className=" flex flex-col justify-between">
+      <p>
+        I'm having the issue that's faced in this question: Invalid options object. Dev Server has
+        been initialized using an options object that does not match the API schema One of the
+        solutions is to downgrade react-scripts to 4.0.3 and I'm not sure how to do that. Could
+        someone help me?
+      </p>
+
+      {/* button 컴포넌트 분리할지 안할지 */}
+      <div className=" flex gap-4 text-sm">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div>
   );
 };
