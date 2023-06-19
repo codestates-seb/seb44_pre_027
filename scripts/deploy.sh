071e4ea805fe6237034adaede08f94a960c90404
[haru lee] <hitenshi1997@gmail.com>
2023-06-19 22:33:43 +0900

    fix: gradle.yml 외 2개 re-fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# 빌드 파일의 이름이 콘텐츠와 다르다면 다음 줄의 .jar 파일 이름을 수정하시기 바랍니다.
+BUILD_JAR=$(ls /home/ubuntu/action/server/build/libs/stackoverflow-clone-0.0.1-SNAPSHOT.jar)
+JAR_NAME=$(basename $BUILD_JAR)
 
-REPOSITORY=/home/ubuntu/action
-cd $REPOSITORY
+echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
 
-APP_NAME=stackoverflow-clone
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
+DEPLOY_PATH=/home/ubuntu/action/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할 애플리케이션이 없습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
+  sudo kill -9 $CURRENT_PID
   sleep 5
 fi
 
-echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/action/deploy.log 2>/home/ubuntu/action/deploy_err.log &
