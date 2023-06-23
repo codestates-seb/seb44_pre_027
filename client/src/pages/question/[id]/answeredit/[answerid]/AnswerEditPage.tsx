69d3750340645978e03f9e324615a20e3d3ffb79
xion <calf-love@naver.com>
2023-06-23 13:30:47 +0900

    add wrapper

--- client/src/pages/question/[id]/answeredit/[answerid]/AnswerEditPage.tsx
+++ client/src/pages/question/[id]/answeredit/[answerid]/AnswerEditPage.tsx
@@ -1,6 +1,7 @@
 import useQnAPatchMutation, { PatchInterface } from '@/queries/useQnAPatchMutation';
 import { useParams, useNavigate } from 'react-router-dom';
 import React, { useRef } from 'react';
+import Wrapper from '@/common/Wrapper';
 
 interface AnswerEditPageProps {}
 
@@ -39,54 +40,56 @@ const AnswerEditPage = ({}: AnswerEditPageProps) => {
   };
 
   return (
-    <main className="m-5 flex flex-row">
-      <div className="flex flex-col">
-        <div className="flex max-w-[661px] flex-col gap-3 border border-amber-200 bg-amber-100 bg-opacity-50 px-6 pb-6 pt-4 text-sm font-light">
-          {EditDatas.editdescription.map((description) => (
-            <span key={description}>{description}</span>
-          ))}
-        </div>
+    <Wrapper>
+      <main className="m-5 flex flex-row">
+        <div className="flex flex-col">
+          <div className="flex max-w-[661px] flex-col gap-3 border border-amber-200 bg-amber-100 bg-opacity-50 px-6 pb-6 pt-4 text-sm font-light">
+            {EditDatas.editdescription.map((description) => (
+              <span key={description}>{description}</span>
+            ))}
+          </div>
 
-        <div className="mb-2">
-          <div className=" mb-3 mt-4 rounded-sm ">Body</div>
-          <textarea
-            className=" min-h-[243px] w-full overflow-visible rounded-md
+          <div className="mb-2">
+            <div className=" mb-3 mt-4 rounded-sm ">Body</div>
+            <textarea
+              className=" min-h-[243px] w-full overflow-visible rounded-md
                                     border border-slate-300 px-3 py-2 text-xs"
-            ref={bodyRef}
-            required
-          ></textarea>
-        </div>
+              ref={bodyRef}
+              required
+            ></textarea>
+          </div>
 
-        <div className="my-4 mb-3 text-sm ">
-          <button
-            className="rounded border border-zinc-200 bg-blue-500 px-3 
+          <div className="my-4 mb-3 text-sm ">
+            <button
+              className="rounded border border-zinc-200 bg-blue-500 px-3 
                         py-2 font-normal text-white hover:bg-blue-800"
-            onClick={saveEditHandler}
-          >
-            Save edits
-          </button>
-          <button
-            className="mx-2 bg-white px-3 py-2 font-normal 
+              onClick={saveEditHandler}
+            >
+              Save edits
+            </button>
+            <button
+              className="mx-2 bg-white px-3 py-2 font-normal 
                             text-sky-500 hover:rounded hover:bg-sky-50"
-          >
-            Cancel
-          </button>
+            >
+              Cancel
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="ml-5 flex max-h-[223px] min-w-[352px] flex-col  border border-orange-100 bg-amber-100 bg-opacity-50 text-sm md:block md:w-auto">
-        <div className="border-b border-orange-100 bg-amber-100 py-2 pl-5 font-normal">
-          How to Edit
+        <div className="ml-5 flex max-h-[223px] min-w-[352px] flex-col  border border-orange-100 bg-amber-100 bg-opacity-50 text-sm md:block md:w-auto">
+          <div className="border-b border-orange-100 bg-amber-100 py-2 pl-5 font-normal">
+            How to Edit
+          </div>
+          <ul className="list-disc py-2 pl-8 text-xs">
+            {EditDatas.howtoeditlist.map((edit) => (
+              <li className=" py-2 pl-2" key={edit}>
+                {edit}
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="list-disc py-2 pl-8 text-xs">
-          {EditDatas.howtoeditlist.map((edit) => (
-            <li className=" py-2 pl-2" key={edit}>
-              {edit}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </main>
+      </main>
+    </Wrapper>
   );
 };
 
