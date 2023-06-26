d9a9a7e4a34c196fa893634b3a85fa3f51c539e5
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-06-26 15:58:39 +0900

    Feat/회원만 답변 작성 가능하도록 수정

--- client/src/pages/question/[id]/QuestionDetailPage.tsx
+++ client/src/pages/question/[id]/QuestionDetailPage.tsx
@@ -17,12 +17,15 @@ import { ColumnItemWrapper } from '@/common/style/Containers.styled';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { PrimaryBtn } from '@/common/style/Buttons.styled';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { RootState } from '@/modules/store';
+import { useSelector } from 'react-redux';
 
 interface QuestionDetailPageProps {}
 
 const QuestionDetailPage = ({}: QuestionDetailPageProps) => {
   const { register, handleSubmit, setValue } = useForm();
   const { questionid } = useParams();
+  const isUser = useSelector((state: RootState) => state.login);
 
   let onlyQuestionData = {
     questionId: 0,
@@ -46,12 +49,18 @@ const QuestionDetailPage = ({}: QuestionDetailPageProps) => {
 
   const onSubmitAnswer: SubmitHandler<FieldValues> = useCallback(
     (data:FieldValues) => {
-      mutation.mutate(data, {
-        onSettled: ()=>{
-          setValue('content', '');
-          refetch();
-        }
-      });
+      if(isUser.isLogin){
+        mutation.mutate(data, {
+          onSettled: ()=>{
+            setValue('content', '');
+            refetch();
+          }
+        });
+      }
+      else {
+        alert('회원만 답변 작성이 가능합니다.');
+        setValue('content', '');
+      }
     },
     [mutation]
     )
